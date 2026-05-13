0f0f4623ad9f4676d4683ec54b7abdc69e5f90b9
Tanmay Shingavi <shingavitanmay@yahoo.com>
2026-05-13 20:36:20 +0530

    added module crypto.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
 const { Boom } = require('@hapi/boom');
 const pino = require('pino');
